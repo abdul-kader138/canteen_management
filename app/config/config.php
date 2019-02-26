434dc96cb2c3e43abcfdffdb017aa621f443307a
abdul-kader138 <babu313136@gmail.com>
2019-02-26 17:19:39 +0600

    Calender Adding

--- app/config/config.php
+++ app/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://10.1.1.55/p/';
+$config['base_url'] = 'http://localhost/canteen_management/';
 
 /*
 |--------------------------------------------------------------------------
